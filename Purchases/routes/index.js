c33bf6e43a6b87061037f9acdab2b1a3a0e51305
Aushtiin <aotangban@gmail.com>
2020-09-21 11:29:49 +0100

    updated purchase routes

--- Purchases/routes/index.js
+++ Purchases/routes/index.js
@@ -2,13 +2,18 @@ const express = require('express');
 const catchErrors = require('../../middleware/catcherrors');
 const validate = require('../../middleware/validate');
 const { validatePurchase } = require('../../models/purchase');
-const { newPurchase } = require('../controllers');
+const { newPurchase, getPurchase } = require('../controllers');
 const router = express.Router();
 
 router.post(
     '/',
     validate(validatePurchase),
     catchErrors(newPurchase)
+)
+
+router.get(
+    "/:id",
+    catchErrors(getPurchase)
 )
 
 
